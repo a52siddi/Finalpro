8bfb4c9f5923173660b82d92693e24a146e92df7
a52siddi <abd_siddiqui@yahoo.com>
2013-11-22 20:04:45 -0500

    thbfe

---
+++ Finalpro/src/Finalpro.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Finalpro.cpp
+// Author      : a52siddi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "Final project for BME506"<< endl;
+
+	return 0;
+}
