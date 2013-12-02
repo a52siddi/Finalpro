9de336305ed7580561fc45d0060727ccd77b0528
a52siddi <abd_siddiqui@yahoo.com>
2013-12-02 01:26:37 -0500

    Final bloodtests.

--- Finalpro/src/bloodtests.hpp
+++ Finalpro/src/bloodtests.hpp
@@ -11,26 +11,51 @@
 #include<string>
 using namespace std;
 class bloodtests{
-	double sodiumconc;
-	double potassiumconcen;
-	double chlorideconcen;
-	int pH;
-	double Olevel;
-	double bicarblevel;
-	double redbloodcell;
-	double whitebloodcell;
-	double platelets;
-	double hemoglobin;
-	double hematocrit;
-	double meancorpuscularvolume;
-	double cholestrol_low;
-	double cholestrol_high;
-	double cholestrol_total;
+	string firstname;
+	string lastname;
+	int age;
+	char gender;
 
 public:
-	void Electrolytes();
-	void Completebloodcount();
-	void Lipoprotein();
+	void Information();
+
+};
+
+class Electrolytes: public bloodtests{
+	double sodiumconc;
+		double potassiumconcen;
+		double chlorideconcen;
+		int pH;
+		double Olevel;
+		double bicarblevel;
+
+public:
+		void ionlevel(); //asks for concentration of sodium, potassium, chloride, bicarbonate ions and oxygen level of blood and provides feedback
+	     void pH(); //asks for pH levels and provides feedback.
+};
+
+class Completebloodcount: public bloodtests{
+	double redbloodcell;
+		double whitebloodcell;
+		double plat;
+		double hemo;
+		double hemat;
+		double volume;
+public:
+		void bloodcellcount(); //asks for blood cell count and provides feedback.
+		void platelets (); //uses plat as input and provides feedback.
+		void Hemoglobin (); //uses hemo as input and provides feedback.
+		void Hematocrit (); //uses hemat as input and provides feedback.
+		void meancorpuscularvolume (); //uses volume as input and provides feedback.
+
+};
+
+class Lipoprotein: public bloodtests{
+	double cholestrol_low;
+		double cholestrol_high;
+		double cholestrol_total;
+public:
+		void Cholestrol(); //uses the above values as input to provide feedback.
 
 };
 
